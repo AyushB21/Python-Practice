18fa6a2133f7e96af29217505f419d6960a3ffd0
AyushB21 <ayushbh0612@gmail.com>
2024-10-09 18:14:22 +0530

    =Added files

---
+++ Code Practice 1/ReviewExercise3.py
@@ -0,0 +1,6 @@
+sumeven=0
+for i in range(11):
+    if i%2==0:
+        sumeven=sumeven+i
+print(sumeven)
+        
---
+++ Code Practice 1/ReviewExercise4.py
@@ -0,0 +1,6 @@
+sumnum=0
+for i in range(1,21):
+    if i%2!=0 or i%3!=0 or i%5!=0:
+        sumnum+=i
+
+print(sumnum)
