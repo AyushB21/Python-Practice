24082afc2208882f2fe1d13d6fd4f5cca9682abe
AyushB21 <ayushbh0612@gmail.com>
2024-10-11 19:34:14 +0530

    =Added files

---
+++ Code Practice 1/right shift operation without operator.py
@@ -0,0 +1,4 @@
+n=int(input("Enter a number: "))
+s=int(input("Enter number of bits to be shift right: "))
+y=n/(2**s)
+print(n,'>>',s,'=',y)
---
+++ Code Practice 1/scipy_mode.py
@@ -0,0 +1,5 @@
+from scipy import stats
+
+speed=[99,86,87,81,99,101,82,111]
+x=stats.mode(speed)
+print(x)
