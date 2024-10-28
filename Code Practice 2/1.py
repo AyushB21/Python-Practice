0fdd51561cd910b51ffc11ed7cbeecc8dcdd65b8
AyushB21 <ayushbh0612@gmail.com>
2024-10-28 17:38:40 +0530

    =Added files

---
+++ Code Practice 2/1.py
@@ -0,0 +1,10 @@
+a=int(input("Enter first number: "))
+b=int(input("Enter second number: "))
+c=int(input("Enter third number: "))
+if a>b and a>c:
+    print("A:",a ,"is greatest")
+
+elif b>c and b>a:
+    print("B:",b ,"is greatest")
+else:
+    print("C:",c ,"is greatest")
