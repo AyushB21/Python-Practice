5f5c1d802188ad8dee8cebd2920167e848a86719
AyushB21 <ayushbh0612@gmail.com>
2024-11-27 23:23:41 +0530

    =Added files

---
+++ Code Practice 2/User made packages.py
@@ -0,0 +1,9 @@
+import Package1.math12
+import Package1.math13
+a=int(input("Enter a: "))
+b=int(input("Enter b: "))
+s=Package1.math12.add1(a,b)
+d=Package1.math12.div1(a,b)
+m=Package1.math12.mult1(a,b)
+p=Package1.math13.pow1(a,b)
+print(s,d,m,p,sep='\n')
