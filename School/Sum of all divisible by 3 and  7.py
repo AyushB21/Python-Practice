c3f5317ac91e0efd0b4b61ec659b629b71b3fc07
AyushB21 <ayushbh0612@gmail.com>
2024-12-25 20:13:25 +0530

    =Added files

---
+++ School/Sum of all divisible by 3 and  7.py
@@ -0,0 +1,6 @@
+li=[14,21,52,77,42,96]
+s=0
+for i in li:
+    if i%3==0 and i%7==0:
+        s=s+i
+print(s)
---
+++ School/Sum of those whose last number is 5.py
@@ -0,0 +1,7 @@
+li=[17,52,85,19,7,5,135,35]
+s=0
+for i in li:
+    if i%10==5:
+        print(i)
+        s=s+i
+print(s)
